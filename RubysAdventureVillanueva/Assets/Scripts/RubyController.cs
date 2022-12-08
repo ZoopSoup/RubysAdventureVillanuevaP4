6c46ffa915b026b18c2ca0f8e7b74799531d5bc4
ZoopSoup <zoopidysoupidy@gmail.com>
2022-12-08 11:43:41 -0800

    Added Collision to Ruby And MetalBox
    Added Collision to ruby and metal box and started to add collision to the tilemap

--- RubysAdventureVillanueva/Assets/Scripts/RubyController.cs
+++ RubysAdventureVillanueva/Assets/Scripts/RubyController.cs
@@ -4,28 +4,30 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //create 2 variables to use the unity build in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //Create my movement vector variable
-        Vector2 position = transform.position;
-
-        //Create the movement of my character
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //Set the new position
-        transform.position = position;
-
+        rigidbody2d.MovePosition(position);
     }
 }
